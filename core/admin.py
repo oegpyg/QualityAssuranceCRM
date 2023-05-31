561ac89ff894ffcf2204127bf146473af593fd06
oegpyg <oegpyg@gmail.com>
2023-05-31 14:29:08 -0400

    add custom admin model view example

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Status
 
-admin.site.register(Status)
+
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = ['label', 'target_flow']
+    list_filter = ['target_flow']
+    list_display = ['id', 'label', 'target_flow']
+
+
+admin.site.register(Status, StatusAdmin)
