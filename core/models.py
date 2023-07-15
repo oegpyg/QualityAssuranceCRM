f52fd4d25619088a477acb54bd3078bc12fff11c
Monse <thelmon97@gmail.com>
2023-07-15 10:37:36 -0400

    creando las tablas

--- core/models.py
+++ core/models.py
@@ -17,18 +17,32 @@ class Status(models.Model):
         list_filter = ['target_flow']
         list_display = ['id', 'label', 'target_flow']
 
-
+class HistoricStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.ForeignKey(Status)
+    
 
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    fecha_inicio = models.DateField()
-    """usuario 
-    techlead
-    owner
-    developer
-    architech""" 
+    """se puede poner como un campo obligatorio de historia de usuario?"""
+    startDate = models.DateField()
+    creator = models.models.TextField()
+    """ tendria que ser el id de quien crea la incidencia """
+    status = models.ForeignKey(Status)
+    priority = models.TextField()  
+    """ se podria setear especificos?, ejemplo: alta, media, baja """
+    productOwner = models.TextField()
+    developer = models.TextField()
+    qa = models.TextField()
+    stakeholder = models.TextField() 
+    assignedTo = models.TextField()
+    """como hacer que cuando escriba el inicio del nombre le estire las opciones de users ya creados?"""
+    version = models.enums ()
+    businessUnit = models.CharField()
+    TypeOfTests = models.TextField()  
+    """ se podria setear especificos?, ejemplo: end2end, unitarias, UAT, rendimiento, integración, monitoreo, seguridad, migracion de datos """
 
     def __str__(self):
         return f'{self.id} - {self.title}'
@@ -37,73 +51,127 @@ class Project(models.Model):
         list_display = ['id', 'title', 'description', 'view_releases_link']
 
         def view_releases_link(self, obj):
-            count = obj.release_set.count()
-            url = (
+          count = obj.release_set.count()
+          url = (
                     reverse("admin:core_release_changelist")
-                    + "?"
-                    + urlencode({"project__id": f"{obj.id}"})
-            )
-            return format_html('<a href="{}">{} Releases</a>', url, count)
+                  + "?"
+                  + urlencode({"project__id": f"{obj.id}"})
+                 )
+    
 
 class Release(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    assignedTo
-    
+    status = models.ForeignKey(Status)
+    creator = models.models.TextField()
+    """ tendria que ser el id de quien crea la incidencia """
+    assignedTo = models.TextField()
+    """como hacer que cuando escriba el inicio del nombre le estire las opciones de users ya creados?"""
+    priority = models.TextField()  
+    """ se podria setear especificos?, ejemplo: alta, media, baja """
+    version = models.enums ()
+    businessUnit = models.CharField()
+    LinkPlatform = models.FileField()
+    PlatformAffected = models.CharField()
+    TypeOfTests = models.TextField()  
+    """ se podria setear especificos?, ejemplo: end2end, unitarias, UAT, rendimiento, integración, monitoreo, seguridad, migracion de datos """
+    BusinessAreaAffected = models.CharField()
+    """contabilidad, ventas, etc"""
+    CommercialApproval = models.CharField(max_length=2)
+    """si o no"""
+    QaDeploymentPlanningDate = models.DateField()
+    TestPlanCreationDate= models.DateField()
+    TestStartDate = models.DateField()
+    TestEndDate = models.DateField()
+    PlannedImplementationDate= models.DateField()
+    FinalImplementationDate= models.DateField()
+    productOwner = models.TextField()
+    """solo usuarios creados, no deberia de poder escribirse cualquier nombre"""
+    developer = models.TextField()
+    """solo usuarios creados, no deberia de poder escribirse cualquier nombre"""
+    qa = models.TextField()
+    """solo usuarios creados, no deberia de poder escribirse cualquier nombre"""
+
     def __str__(self):
         return f'{self.id} - {self.title}'
     
     class Admin(admin.ModelAdmin):
         list_display = ['id', 'title', 'description']
 
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=8)
+    email = models.EmailField()
+    phonenumber = models.models.PhoneNumberField()
+    profile = models.ForeignKey(perfilUsuarios)
+
+class perfilUsuarios (models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.TextField()
+
+class input (models.Model):
+    id = models.AutoField(primary_key=True)
+    caption = models.CharField(max_length=50)
+    TypeInput = models.CharField()
+    """En alguna parte se tendria que poder crear y elegir el tipo por ejemplo tarea, historia de usuario"""
+    AssociatedRealease = models.ForeignKey(Release)
+    status = models.ForeignKey(Status)
+    assignedTo = models.TextField()
+    """como hacer que cuando escriba el inicio del nombre le estire las opciones de users ya creados?"""
+    description = models.TextField()
+    comments = models.models.TextField()
+
+class QaDocumentation(models.Model):
+    id = models.AutoField(primary_key=True)
+    TestPlans = models.TextField()
+    ProductOwnerApproval = models.TextField()
+    """esto podria ser por ejemplo un check que solo tenga permiso de modificar ese perfil? para ahorrar tiempo en no enviar correos?"""
+    DeveloperApproval = models.TextField()
+    ImportanceOfTestCases = models.TextField()
+    ChecklistTestTypes = models.TextField()
+    EvidenceOfTheTestPlans = models.TextField()
+    
+class ReportedBugs(models.Model):
+    id = models.AutoField(primary_key=True)
+    caption = models.CharField(max_length=50)
+    TypeInput = models.CharField()
+    ReportedBy = models.CharField()
+    """deberia aceptar solo usuarios creados"""
+    assignedTo = models.TextField()
+    """mismo caso de solo usuarios creados"""
+    StatusBugs = models.TextChoices("Nuevo", "Se necesitan más datos", "Asignado", "Resuelto", "Cerrado")
+    Category = models.TextChoices("Funcionalidad no disponible", "Manejo de errores", "Implementacion incorrecta", "Mejora", "Observación", "Seguridad", "Interfaz de usuario", "Usabilidad", "Navegabilidad")
+    Priority = models.TextChoices("Bloqueo", "Urgente", "Alta", "Media", "Baja")
+    Severity = models.TextChoices("Crítico", "Mayor", "Menor", "Sugerencia")
+    Reproducibility = models.TextChoices("Siempre", "A veces", "Aleatorio", "No se ha intentado", "No es reproducible", "Desconocido")
+    Summary = models.CharField(max_length=50)
+    Description = models.TextField()
+    StepsToReproduce = models.TextField()
+    """subir archivos se podria? por ejemplo print de pantalla, videos"""
+
+class ImplementationRelease(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.models.TextField()
+    idRelease = models.ForeignKey(Release)
+    ImplementationDate= models.DateField()
+    Documentation = models.models.models.ForeignKey(QaDocumentation)
+    developerInCharge = models.TextField()
+    qaInCharge = models.TextField()
+    Results = models.TextChoices("Exitoso", "Fallido")
+    DescriptionResults = models-models.TextField()
+    TestEvidence = models.models.CharField(max_length=50)
+    """poder adjuntar imagenes, videos"""
+
 """
-Tablas
-PerfilUsuarios: id, chartlenght, permisos?
- Usuarios: id, chartlenght, user, password, id_PerfilUsuarios
-Proyectos: id, chartlenght, id_entregas, id_EstadoIncidencias……. creador, estado, prioridad, asignado a, versión, unidad de negocio, tipos de pruebas (end2end, rendimiento, usabilidad, portabilidad),líder técnico, solicitante. 
-Entregas: id, chartlenght, id_Proyectos, id_EstadoIncidencias……. creador, estado, prioridad, asignado a, versión, unidad de negocio, link evidencias, plataformas afectadas (app mobile, app web, crm1, crm2, base de datos X) , área de prueba (digital, x, y), tipos de pruebas (end2end, rendimiento, usabilidad, portabilidad), planificación despliegue en QA, fecha de implementación planificada, fecha implementación final, líder técnico, solicitante, aprobación comercial necesaria.
-    Tipo_Entrada:  id, chartlenght.
-Incidencias: id, chartlenght, id_Estado_Incidencias
-EstadoIncidencias:  id, chartlenght, id_incidencias
-Histórico_estados:  id, chartlenght, id_EstadoIncidencias
-    PersonaAsignada: id, chartlenght 
-CampoComentarios: chartlenght, id_incidencias 
-DocumentacionProyecto: id, chartlenght, historias de usuario
-DocumentacionEntrega: id, chartlenght, documentaciones qa? plan de pruebas, aprobación del plan de pruebas, checklist tipos de pruebas, importancia de casos de prueba, evidencias, cierre, planilla de riesgos.  
-ErroresReportados:  id, chartlenght, detalle, responsable, campo para adjuntar imagen??
-    Estado_Error:  id, chartlenght, id_ErroresReportados
-Implementación_producción:  id, chartlenght, date?, id_DocumentaciónEntrega, resultado?, id_usuarios (a cargo) 
-search: id_Proyectos, id_Entregas, id_Incidencias, id_EstadoIncidencias, id_PersonaAsignada, chartlenght_Proyectos, chartlenght_Entregas, chartlenght_Incidencias, chartlenght_EstadoIncidencias, chartlenght_PersonaAsignada.
-
-
-
 Preguntas
 1- para la parte en donde se carguen las evidencias tambien se necesitarian tablas?
 2- para enviar notificaciones a las personas cuando cambia de estado un error?
-
-
-
-class Buscador? (admin.ModelBuscador):
-
-
-
-
-
-
-
-
-
-models: crear tablas necesarias PRIMERO
-para crear tablas es en models.py
-class ProductAdmin(admin.ModelAdmin):
-search_algo para buscar
-lostfilter: filtrar
-list_display: son columnas que se listan en el menu principal
-
-en admin
-admin.site.register(nombre de la clase de la tabla)
-
+3- en que parte se agrega el buscador? en cada tabla o se hace una tabla aparte? Seria esto mas o menos:
+search: id_Proyectos, id_Entregas, id_Incidencias, id_EstadoIncidencias, id_PersonaAsignada, 
+chartlenght_Proyectos, chartlenght_Entregas, chartlenght_Incidencias, chartlenght_EstadoIncidencias, 
+chartlenght_PersonaAsignada.
 
 """
