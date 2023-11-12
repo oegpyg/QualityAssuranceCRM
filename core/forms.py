6c144310d22d68bde2fa1664fd9fb10cb0daffa9
oegpyg <oegpyg@gmail.com>
2023-11-12 13:52:11 -0300

    add widgets to models forms

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea, NumberInput, FileInput, DateInput, DateTimeInput, TimeInput, CheckboxInput, Select
 from .models import Project
 
 
@@ -6,3 +6,8 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ["title", "description", "startDate"]
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': "Nombre del Proyecto"}),
+            'description': Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripcion', 'cols': 80, 'rows': 20}),
+            'startDate': DateInput(attrs={'class': 'form-control'})
+        }
